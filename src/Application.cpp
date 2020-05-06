36ca70b53e647d9dbaaf85d049dbdf3c89ded072
Evandro Rodrigues <evandro.rodriguescc@gmail.com>
2020-05-06 17:18:00 -0300

    adding textures and fixing some minor issues

--- src/Application.cpp
+++ src/Application.cpp
@@ -13,6 +13,7 @@
 #include "IndexBuffer.h"
 #include "VertexArray.h"
 #include "Shader.h"
+#include "Texture.h"
 
 int main(void) {
     GLFWwindow* window;
@@ -26,7 +27,7 @@ int main(void) {
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(640, 480, "OPENGL STUDIES", NULL, NULL);
     if (!window) {
         glfwTerminate();
         return -1;
@@ -42,10 +43,10 @@ int main(void) {
     std::cout << glGetString(GL_VERSION) << std::endl;
     {
         float positions[] = {
-                -0.8f, -0.8f, // 0
-                0.8f, -0.8f, // 1
-                0.8f, 0.8f, // 2
-                -0.8f, 0.8f, // 3
+                -0.5f, -0.5f, 0.0f, 0.0f, // 0
+                 0.5f, -0.5f, 1.0f, 0.0f, // 1
+                 0.5f,  0.5f, 1.0f, 1.0f, // 2
+                -0.5f,  0.5f, 0.0f, 1.0f, // 3
         };
 
         unsigned int indices[] = {
@@ -53,10 +54,14 @@ int main(void) {
                 2, 3, 0
         };
 
+        GLCall(glEnable(GL_BLEND));
+        GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+
         VertexArray va;
-        VertexBuffer vb(positions, 4 * 2 * sizeof(float));
+        VertexBuffer vb(positions, 4 * 4 * sizeof(float));
 
         VertexBufferLayout layout;
+        layout.Push<float>(2);
         layout.Push<float>(2);
         va.AddBuffer(vb, layout);
 
@@ -64,7 +69,11 @@ int main(void) {
 
         Shader shader("../res/shaders/Basic.shader");
         shader.Bind();
-        shader.SetUniform4f("u_Color", -.7f, .7f, .7f, .0f);
+        //shader.SetUniform4f("u_Color", -.7f, .7f, .7f, .0f);
+
+        Texture texture("../res/textures/hl_logo.png");
+        texture.Bind();
+        shader.SetUniform1i("u_Texture", 0);
 
         va.Unbind();
         vb.Unbind();
@@ -81,7 +90,7 @@ int main(void) {
             renderer.Clear();
 
             shader.Bind();
-            shader.SetUniform4f("u_Color", r, .3f, .7f, .0f);
+            //shader.SetUniform4f("u_Color", r, .3f, .7f, .0f);
 
             renderer.Draw(va, ib, shader);
 
