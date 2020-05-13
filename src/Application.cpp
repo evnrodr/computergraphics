b0a65b637a7a8e718133ded0a3e07fb4043213aa
Evandro Rodrigues <evandro.rodriguescc@gmail.com>
2020-05-12 23:29:36 -0300

    adding matrix projection

--- src/Application.cpp
+++ src/Application.cpp
@@ -30,7 +30,8 @@ int main(void) {
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "OPENGL STUDIES", NULL, NULL);
+    window = glfwCreateWindow(960, 540, "OPENGL STUDIES", NULL, NULL);
+
     if (!window) {
         glfwTerminate();
         return -1;
@@ -46,10 +47,10 @@ int main(void) {
     std::cout << glGetString(GL_VERSION) << std::endl;
     {
         float positions[] = {
-                -0.5f, -0.5f, 0.0f, 0.0f, // 0
-                 0.5f, -0.5f, 1.0f, 0.0f, // 1
-                 0.5f,  0.5f, 1.0f, 1.0f, // 2
-                -0.5f,  0.5f, 0.0f, 1.0f, // 3
+                100.0f, 100.0f, 0.0f, 0.0f, // 0
+                200.0f, 100.0f, 1.0f, 0.0f, // 1
+                200.0f, 200.0f, 1.0f, 1.0f, // 2
+                100.0f, 200.0f, 0.0f, 1.0f, // 3
         };
 
         unsigned int indices[] = {
@@ -70,7 +71,10 @@ int main(void) {
 
         IndexBuffer ib(indices, 6);
 
-        glm::mat4 proj = glm::ortho(-2.0f, 2.0f, -1.5f, 1.5f, -1.0f, 1.0f);
+        glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
+        glm::vec4 vp(100.0f, 100.0f, 0.0f, 1.0f);
+
+        glm::vec4 result = proj * vp;
 
         Shader shader("../res/shaders/Basic.shader");
         shader.Bind();
